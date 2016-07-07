8cb7883fede3aac730f5ef4e896a24ecb396f32d
Tom Dale <tom@tomdale.net>
2016-07-07 11:11:55 -0400

    WIP on CommonJS output

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 var stew = require('broccoli-stew');
 var mergeTrees = require('broccoli-merge-trees');
 var rollup = require('broccoli-rollup');
+var assert = require('assert');
 
 module.exports = function(options) {
   options = options || {};
@@ -13,6 +14,26 @@ module.exports = function(options) {
   var projectName = getPackageName(projectPath);
   var srcPath = path.join(projectPath, 'src');
 
+  var targets = options.targets || ['js', 'cjs', 'umd'];
+
+  assert.ok(targets instanceof Array, 'targets option must be an array of target formats');
+  normalizeTargets(targets);
+
+  var outputTrees = [];
+
+  function hasTarget(target) {
+    return targets.indexOf(target) > -1;
+  }
+
+  // First, transpile the source TypeScript/ES2015+ JavaScript into ES5.
+  // We'll create two versions:
+  //
+  // * A version with ES2015 module syntax, for the browser.
+  //   (All other browser-targeted builds are derived from this using
+  //   Rollup).
+  // * A version with CommonJS modules, for Node.js.
+
+  // Create a Broccoli tree for all input JavaScript and TypeScript files.
   var src = stew.find(srcPath, {
     include: [
       '**/*.js',
@@ -20,30 +41,69 @@ module.exports = function(options) {
     ]
   });
 
-  var compiled = typeScript(src, {
+  var js = typeScript(src, {
     tsconfig: {
-      "compilerOptions": {
-        "target": "ES6"
+      compilerOptions: {
+        target: 'es5',
+        module: 'es2015',
+        sourceMaps: 'inline'
       }
     }
   });
 
-  var es6 = stew.mv(compiled, 'es6');
+  if (hasTarget('js')) {
+    outputTrees.push(stew.mv(js, 'js'));
+  }
 
-  var umd = rollup(compiled, {
-    inputFiles: ['**/*.js'],
-    rollup: {
-      format: 'umd',
-      moduleName: options.moduleName || camelize(projectName),
-      entry: options.entry || 'index.js',
-      dest: projectName + '.js'
-    }
-  });
+  if (hasTarget('cjs')) {
+    var cjs = typeScript(src, {
+      tsconfig: {
+        compilerOptions: {
+          target: 'es5',
+          module: 'commonjs',
+          sourceMap: true,
+          inlineSourceMap: true
+        }
+      }
+    });
 
-  umd = stew.mv(umd, 'umd');
+    cjs = stew.mv(cjs, 'cjs');
 
-  return mergeTrees([es6, umd]);
+    outputTrees.push(cjs);
+  }
+
+  if (hasTarget('umd')) {
+    var umd = rollup(js, {
+      inputFiles: ['**/*.js'],
+      rollup: {
+        format: 'umd',
+        moduleName: options.moduleName || camelize(projectName),
+        entry: options.entry || 'index.js',
+        dest: projectName + '.js'
+      }
+    });
+
+    umd = stew.mv(umd, 'umd');
+
+    outputTrees.push(umd);
+  }
+
+  return mergeTrees(outputTrees);
 };
+
+function normalizeTargets(targets) {
+  for (var i = 0; i < targets.length; i++) {
+    switch (targets[i]) {
+      case 'es6':
+      case 'es2015':
+        targets[i] = 'js';
+        break;
+      case 'commonjs':
+        targets[i] = 'cjs';
+        break;
+    }
+  }
+}
 
 function getPackageName(projectPath) {
   return JSON.parse(fs.readFileSync(path.join(projectPath, 'package.json'))).name;
