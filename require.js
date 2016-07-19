1e6e96514efa7485ffa1827b6cf4f2bf9b231cbf
Tom Dale <tom@tomdale.net>
2016-07-19 18:54:14 -0400

    Add module requiring helper

---
+++ require.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var findup = require('findup-sync');
+
+module.exports = function(requirePath) {
+  var pkgPath = findup('package.json', { cwd: path.dirname(module.parent.filename) });
+  pkgPath = path.dirname(pkgPath);
+
+  try {
+    return require(path.join(pkgPath, 'src', requirePath));
+  } catch (e) {
+    return require(path.join(pkgPath, 'cjs', requirePath));
+  }
+};
